417832dfc0b9a11f7d101675168738c1a71cf478
Theogorath <theo.drezenugurlu@viacesi.fr>
2017-06-25 17:16:14 +0200

    fillMapLevel as comment

--- BoulderDash/model/src/main/java/model/Map.java
+++ BoulderDash/model/src/main/java/model/Map.java
@@ -32,6 +32,23 @@ public class Map implements IMap{
 		}
 	}
 	
+	/*
+		public void fillMapLevel(){
+		Connection connect = new Connection();
+		this.mapLevel = new Element[line][colomn];
+		int[][] table = new int[line][colomn];
+		connect.getMapByLevelId(levelId, table);
+		for(int i = 0; i < line; i++){
+			for(int j = 0; j < colomn; j++){
+				Point position = new Point(i, j);
+				int id = table[i][j];
+				mapLevel[i][j] = new Element(id, position);
+			}	
+		}
+	}
+
+	*/
+	
 	public void setTable(Element[][] table) {
 		this.mapLevel = table;
 	}
