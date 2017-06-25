df9d5435fce277e16b957cb52161cf8694d78879
EvaPierrini <eva.pierrini@viacesi.fr>
2017-06-25 18:31:26 +0200

    element try catch

--- BoulderDash/model/src/main/java/model/Element.java
+++ BoulderDash/model/src/main/java/model/Element.java
@@ -1,49 +1,4 @@
-<<<<<<< HEAD
-package model;
-import java.awt.Point;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
-/**
- * <h1>The Class Element, mother class of Static and Mobile</h1>
- *
- * @author Hugo, Théo, Eva and Geoffrey
- * @version 1.0
- */
-public class Element implements IElement {
-	private int id;
-	private BufferedImage image;
-	private BufferedImage img;
-	
-	public Element(int id) throws IOException{
-		this.id = id;
-		this.img = ImageIO.read(new File("C:\\Users\\Ghost64\\Desktop\\images\\blocks.png"));
-		this.image = img.getSubimage(32, 0, 16, 16);
-		 
-		
-		
-	}
-	
-	public BufferedImage getImage() {
-		return image;
-	}
-
-	public synchronized void setImage(BufferedImage image) {
-		this.image = image;
-	}
-
-	
-	public synchronized int getId() {
-		return this.id;
-	}
-	
-	
-	
-}
-=======
 package model;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
@@ -86,4 +41,4 @@ public class Element implements IElement {
 	
 	
 }
->>>>>>> branch 'master' of https://github.com/Ghostcatcher/BoulderDashProject.git
+
--- BoulderDash/model/src/main/java/model/Map.java
+++ BoulderDash/model/src/main/java/model/Map.java
@@ -1,60 +1,8 @@
-<<<<<<< HEAD
 package model;
 
 import java.io.IOException;
 
-import model.dao.Connection;
 
-public class Map implements IMap{
-	private IElement[][] mapLevel;
-	static private int line = 22;
-	static private int colomn = 40;
-	static private int levelId = 1;
-	
-	public Map() throws IOException{
-		this.fillMapLevel();
-	}
-	
-	public void fillMapLevel() throws IOException{
-		
-		Connection connect = new Connection();
-		this.mapLevel = new Element[50][50];
-		int[][] table = new int[line][colomn];
-		table = connect.getMapByLevelId(levelId, table);
-		
-		for(int i = 0; i <= line; i++){	
-			
-			for(int j = 0; j < colomn; j++){
-				int id = table[i][j];
-				mapLevel[i][j] = new Element(id);
-								
-			}	
-		}
-	}
-	
-	public void setTable(Element[][] table) {
-		this.mapLevel = table;
-	}
-
-	public IElement[][] getTable() {
-		return mapLevel;
-	}
-
-	public synchronized static int getLine() {
-		return line;
-	}
-
-	public synchronized static int getColomn() {
-		return colomn;
-	}
-	
-	public synchronized IElement getValue(int a, int b){
-		return mapLevel[a][b];
-	}
-=======
-package model;
-
-import java.io.IOException;
 import model.dao.Connection;
 
 /**
@@ -74,19 +22,6 @@ public class Map implements IMap{
 		this.fillMapLevel();
 	}
 	
-	public void fillMapLevel() throws IOException{
-		Connection connect = new Connection();
-		this.mapLevel = new Element[50][50]; 
-		int[][] table = new int[line][colomn];
-		table = connect.getMapByLevelId(levelId, table);
-		for(int i = 0; i <= line; i++){
-			for(int j = 0; j <= colomn; j++){
-				mapLevel[i][j] = new Element(1);
-			}
-		}
-	}
-	
-	/*
 		public void fillMapLevel(){
 		Connection connect = new Connection();
 		this.mapLevel = new Element[line][colomn];
@@ -101,7 +36,7 @@ public class Map implements IMap{
 		}
 	}
 
-	*/
+
 	
 	public void setTable(Element[][] table) {
 		this.mapLevel = table;
@@ -122,5 +57,5 @@ public class Map implements IMap{
 	public synchronized IElement getValue(int a, int b){
 		return mapLevel[a][b];
 	}
->>>>>>> branch 'master' of https://github.com/Ghostcatcher/BoulderDashProject.git
+
 }
