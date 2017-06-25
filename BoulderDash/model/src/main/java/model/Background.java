209802c677f08a4d6474067902ed9a43dbbc4838
HugoLA1 <hugo.leboeuf@viacesi.fr>
2017-06-25 14:40:44 +0200

    Reply to eva test

--- BoulderDash/model/src/main/java/model/Background.java
+++ BoulderDash/model/src/main/java/model/Background.java
@@ -9,14 +9,8 @@ import java.awt.Point;
  * @version 1.0
  */public class Background extends Static {
 
-	public Background(int id, Point position) {
-		super(id, position);
-		// TODO Auto-generated constructor stub
-	}
-
-//
-	//eva test
-//	//add the background sprite
-//	//background visible
-//	
-}
+	 public Background(int id, Point position) {
+		 super(id, position);
+		 // TODO Auto-generated constructor stub
+	 }
+ }
